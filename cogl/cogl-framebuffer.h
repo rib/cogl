6a67550bdf91c707c693cc06ffc32fb313415586
Robert Bragg <robert@linux.intel.com>
2012-02-17 21:46:39 +0000

    Move all types/prototypes from cogl.h -> cogl-context.h
    So we can get to the point where cogl.h is merely an aggregation of
    header includes for the 1.x api this moves all the function prototypes
    and type definitions into cogl-context.h.
    
    Ideally no code internally should ever need to include cogl.h as it just
    represents the public facing header for accessing the 1.x api which
    should only be used by Clutter.

--- cogl/cogl-framebuffer.h
+++ cogl/cogl-framebuffer.h
@@ -28,8 +28,6 @@
 #ifndef __COGL_FRAMEBUFFER_H
 #define __COGL_FRAMEBUFFER_H
 
-#include <cogl/cogl.h>
-
 #include <glib.h>
 
 #ifdef COGL_HAS_WIN32_SUPPORT
@@ -39,6 +37,15 @@
 #if defined (COGL_HAS_EGL_PLATFORM_WAYLAND_SUPPORT)
 #include <wayland-client.h>
 #endif /* COGL_HAS_EGL_PLATFORM_WAYLAND_SUPPORT */
+
+#ifdef COGL_ENABLE_EXPERIMENTAL_2_0_API
+#include <cogl/cogl2-path.h>
+#else
+#include <cogl/cogl-path.h>
+#endif
+
+#include <cogl/cogl-pipeline.h>
+#include <cogl/cogl-indices.h>
 
 G_BEGIN_DECLS
 
@@ -81,6 +88,8 @@ G_BEGIN_DECLS
  */
 
 #ifdef COGL_ENABLE_EXPERIMENTAL_API
+
+typedef struct _CoglFramebuffer CoglFramebuffer;
 
 #define COGL_FRAMEBUFFER(X) ((CoglFramebuffer *)(X))
 
