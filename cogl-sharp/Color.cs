d149dcb02c65c212d4f2e71607fff7081c78ef59
Damien Lespiau <damien.lespiau@intel.com>
2012-09-29 17:48:06 +0100

    cogl-sharp: Create a cogl2-sharp assembly
    This commit includes quite of a bit of the administrative work to make
    cogl2-sharp.dll assembly that can be used to access Cogl API with C#.
    
    Let's start simple and bing CoglColor in this first commit.

---
+++ cogl-sharp/Color.cs
@@ -0,0 +1,55 @@
+/*
+ * Cogl
+ *
+ * An object oriented GL/GLES Abstraction/Utility Layer
+ *
+ * Copyright (C) 2012 Intel Corporation.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Damien Lespiau <damien.lespiau@intel.com>
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Cogl
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Color
+    {
+        public float Red { get; set; }
+        public float Green { get; set; }
+        public float Blue { get; set; }
+        public float Alpha { get; set; }
+
+        [DllImport("cogl2.dll")]
+        private static extern void cogl_color_premultiply(ref Color color);
+
+        public void Premuliply()
+        {
+            cogl_color_premultiply(ref this);
+        }
+
+        [DllImport("cogl2.dll")]
+        private static extern void cogl_color_unpremultiply(ref Color color);
+
+        public void UnPremultiply()
+        {
+            cogl_color_unpremultiply(ref this);
+        }
+    }
+}
